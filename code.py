fd4f028024515fa018334d1ce5ad6713ac9bdc0c
slothurchin <slothurchin@gmail.com>
2025-02-12 17:48:09 +0900

    Update code.py

--- code.py
+++ code.py
@@ -18,7 +18,7 @@ def save_posts(posts):
 
 def main():
     st.set_page_config(page_title="Simple Blog Platform")
-    st.title("📝 My Streamlit Blog")
+    st.title("📝 Our Streamlit Blog")
     
     menu = ["Home", "New Post", "Manage Posts"]
     choice = st.sidebar.selectbox("Menu", menu)
